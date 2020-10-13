d9f2430b11e7cf7958370781859b53ee0811a008
LavaPower <lavapower84@gmail.com>
2020-10-13 11:48:49 +0200

    Show windows in NaOS Bar

--- naos/graphics/entities/naosbar.py
+++ naos/graphics/entities/naosbar.py
@@ -1,21 +1,34 @@
 import pygame
 import os
 
-from naos.utils import Color
+from naos.utils import Color, Font
 
 class NaOSBar:
     def __init__(self):
         self.logo = pygame.image.load(os.path.join(os.path.dirname(__file__), "..", "..", "files", "images", "logo.png")).convert()
         self.logo = pygame.transform.scale(self.logo, (20, 20))
         self.naos = None
+        self.font = Font(bold=True)
 
     def event(self, evt):
-        if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT and self.logo.get_rect(x=5, y=1055).collidepoint(evt.pos[0], evt.pos[1]):
-            self.naos.startmenu.open = not self.naos.startmenu.open
-            return True
+        if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT:
+            if self.logo.get_rect(x=5, y=1055).collidepoint(evt.pos[0], evt.pos[1]):
+                self.naos.startmenu.open = not self.naos.startmenu.open
+                return True
+            
+            for i, window in enumerate(self.naos.windows):
+                if pygame.Rect(40+25*i, 1055, 20, 20).collidepoint(evt.pos[0], evt.pos[1]):
+                    window.open = not window.open
+                    return True
+
         return False
 
     def show(self, screen):
         pygame.draw.rect(screen, Color.from_name("BLACK").get_rgba(), pygame.Rect(-1, 1049, 1922, 32))
         pygame.draw.rect(screen, Color.from_name("GRAY").get_rgba(), pygame.Rect(0, 1050, 1920, 30))
         screen.blit(self.logo, (5, 1055))
+        pygame.draw.line(screen, Color.from_name("BLACK").get_rgba(), (30, 1049), (30, 1080), 3)
+        for i, window in enumerate(self.naos.windows):
+            pygame.draw.rect(screen, Color.from_name("GRAY").lighter(2).get_rgba(), pygame.Rect(40+25*i, 1055, 20, 20))
+            screen.blit(self.font.render(window.title[0]), (40+25*i + (10 - self.font.rendered_size(window.title[0])[0] / 2), 1055 + (10 - self.font.rendered_size(window.title[0])[1] / 2)))
+
--- naos/graphics/entities/window.py
+++ naos/graphics/entities/window.py
@@ -16,6 +16,7 @@ class Window:
         self.drag_offset = [0, 0]
         self.widgets = []
         self.focus = False
+        self.open = True
 
     def add_widget(self, widget):
         if widget.parent is None:
@@ -30,34 +31,37 @@ class Window:
         self.naos.windows.remove(self)
 
     def event(self, evt):
-        if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT and  pygame.Rect(self.x + self.width - 18, self.y+2, 16, 16).collidepoint(evt.pos[0], evt.pos[1]):
-            self.close()
-            return True
-        if evt.type == pygame.MOUSEBUTTONDOWN and evt.button == pygame.BUTTON_LEFT and pygame.Rect(self.x, self.y, self.width, 20).collidepoint(evt.pos[0], evt.pos[1]):
-            self.is_dragged = True
-            self.drag_offset = [evt.pos[0] - self.x, evt.pos[1] - self.y]
-            return True
-        if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT and self.is_dragged:
-            self.is_dragged = False
-            self.drag_offset = [0, 0]
-            return True
-        if evt.type == pygame.MOUSEMOTION and self.is_dragged:
-            self.x = evt.pos[0] - self.drag_offset[0]
-            self.y = evt.pos[1] - self.drag_offset[1]
-            return True
+        if self.open:
+            if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT and  pygame.Rect(self.x + self.width - 18, self.y+2, 16, 16).collidepoint(evt.pos[0], evt.pos[1]):
+                self.close()
+                return True
+            if evt.type == pygame.MOUSEBUTTONDOWN and evt.button == pygame.BUTTON_LEFT and pygame.Rect(self.x, self.y, self.width, 20).collidepoint(evt.pos[0], evt.pos[1]):
+                self.is_dragged = True
+                self.drag_offset = [evt.pos[0] - self.x, evt.pos[1] - self.y]
+                return True
+            if evt.type == pygame.MOUSEBUTTONUP and evt.button == pygame.BUTTON_LEFT and self.is_dragged:
+                self.is_dragged = False
+                self.drag_offset = [0, 0]
+                return True
+            if evt.type == pygame.MOUSEMOTION and self.is_dragged:
+                self.x = evt.pos[0] - self.drag_offset[0]
+                self.y = evt.pos[1] - self.drag_offset[1]
+                return True
 
-        if self.focus:
-            for i in self.widgets:
-                if i.event(evt):
-                    return True
+            if self.focus:
+                for i in self.widgets:
+                    if i.event(evt):
+                        return True
+
         return False
 
     def show(self, screen):
-        pygame.draw.rect(screen, Color.from_name("BLACK").get_rgba(), pygame.Rect(self.x-1, self.y-1, self.width + 2, self.height + 22))
-        pygame.draw.rect(screen, Color.from_name("GRAY").darker(2).get_rgba(), pygame.Rect(self.x, self.y, self.width, 20))
-        pygame.draw.rect(screen, Color.from_name("GRAY").get_rgba(), pygame.Rect(self.x, self.y+20, self.width, self.height))
-        screen.blit(self.font.render(self.title), (self.x + 4, self.y + 2))
-        pygame.draw.rect(screen, Color.from_name("RED").get_rgba(), pygame.Rect(self.x + self.width - 18, self.y+2, 16, 16))
+        if self.open:
+            pygame.draw.rect(screen, Color.from_name("BLACK").get_rgba(), pygame.Rect(self.x-1, self.y-1, self.width + 2, self.height + 22))
+            pygame.draw.rect(screen, Color.from_name("GRAY").darker(2).get_rgba(), pygame.Rect(self.x, self.y, self.width, 20))
+            pygame.draw.rect(screen, Color.from_name("GRAY").get_rgba(), pygame.Rect(self.x, self.y+20, self.width, self.height))
+            screen.blit(self.font.render(self.title), (self.x + 4, self.y + 2))
+            pygame.draw.rect(screen, Color.from_name("RED").get_rgba(), pygame.Rect(self.x + self.width - 18, self.y+2, 16, 16))
 
             x = clamp(self.x, 0, 1920)
             y = clamp(self.y + 20, 0, 1080)
