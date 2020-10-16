628a8466ca6c24f2dabc029258d9e3b314565192
LavaPower <lavapower84@gmail.com>
2020-10-16 17:28:45 +0200

    Create Line

--- naos/graphics/widgets/__init__.py
+++ naos/graphics/widgets/__init__.py
@@ -1,2 +1,3 @@
 from naos.graphics.widgets.label import Label
 from naos.graphics.widgets.button import Button
+from naos.graphics.widgets.line import Line
---
+++ naos/graphics/widgets/line.py
@@ -0,0 +1,16 @@
+import pygame
+
+from naos.graphics.widgets.widget import Widget
+from naos.utils import Color
+
+class Line(Widget):
+    def __init__(self, x, y, xlength=0, ylength=0, width=2, color=Color.from_name("BLACK")):
+        super().__init__(x, y)
+        self.endx = x + xlength
+        self.endy = y + ylength
+        self.width = width
+        self.color = color
+    
+    def show(self, screen):
+        if self.is_showed:
+            pygame.draw.line(screen, self.color.get_rgba(), (self.x, self.y), (self.endx, self.endy))
