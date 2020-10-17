d850f28735fb4fc77be86ef862e1f05d070f4c0a
LavaPower <lavapower84@gmail.com>
2020-10-17 14:54:33 +0200

    Finish ProgramLister

--- naos/naos_core_programs/program_lister.py
+++ naos/naos_core_programs/program_lister.py
@@ -7,7 +7,13 @@ class ProgramLister(Window):
     def __init__(self, naos):
         super(ProgramLister, self).__init__("Programmes", 600, 300)
         self.naos = naos
-        self.add_widget(Label(10, 10, "\n".join(self.naos.program_manager.get_programs_names())))
+        scrollpanel = ScrollPanel(10, 10, 580, 280)
+        for k, v in enumerate(self.naos.program_manager.get_programs_names()):
+            scrollpanel.add_widget(Button(10, 10+(k*50), v, lambda v=v: self.open_program(v), size=(540, 40)))
+        self.add_widget(scrollpanel)
+
+    def open_program(self, v):
+        self.naos.open_window(self.naos.program_manager.get_program(v).get_instance(self.naos))
 
 program = {
     "instance": ProgramLister,
