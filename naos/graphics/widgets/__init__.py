637d58ab9b558cb1438c80a16d871a65f5cd1932
LavaPower <lavapower84@gmail.com>
2020-10-20 14:53:14 +0200

    Create Image

--- naos/graphics/widgets/__init__.py
+++ naos/graphics/widgets/__init__.py
@@ -2,4 +2,5 @@ from naos.graphics.widgets.label import Label
 from naos.graphics.widgets.button import Button
 from naos.graphics.widgets.line import Line
 from naos.graphics.widgets.scrollpanel import ScrollPanel
+from naos.graphics.widgets.image import Image
 from naos.graphics.widgets.entry import Entry
---
+++ naos/graphics/widgets/image.py
@@ -0,0 +1,29 @@
+import pygame
+
+from naos.graphics.widgets.widget import Widget
+
+class Image(Widget):
+    def __init__(self, x, y, sprite, size=None, rotation=0, flipx=False, flipy=False):
+        super(Image, self).__init__(x, y)
+
+        self.sprite = sprite
+        self.size = size
+        self.rotation = rotation
+        self.flipx = flipx
+        self.flipy = flipy
+
+        self.render = None
+        self.update_render()
+    
+    def update_render(self):
+        self.render = pygame.image.load(self.sprite).convert()
+        if self.size is not None:
+            self.render = pygame.transform.scale(self.render, self.size)
+        if self.rotation != 0:
+            self.render = pygame.transform.rotate(self.render, self.rotation)
+        if self.flipx or self.flipy:
+            self.render = pygame.transform.flip(self.render, self.flipx, self.flipy)
+    
+    def show(self, screen):
+        if self.is_showed:
+            screen.blit(self.render, (self.x, self.y))
