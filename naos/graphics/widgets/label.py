e94e71bc71dad9b759895a40297a6ef316ab78ac
LavaPower <lavapower84@gmail.com>
2020-10-21 11:17:52 +0200

    Add background to Label

--- naos/graphics/widgets/label.py
+++ naos/graphics/widgets/label.py
@@ -4,10 +4,11 @@ from naos.graphics.widgets.widget import Widget
 from naos.utils import Font
 
 class Label(Widget):
-    def __init__(self, x, y, text, font=Font(size=16)):
+    def __init__(self, x, y, text, font=Font(size=16), background=None):
         super().__init__(x, y)
         self.text = text
         self.font = font
+        self.background = background
         self.update_render()
     
     def update_render(self):
@@ -21,7 +22,11 @@ class Label(Widget):
     def show(self, screen):
         if self.is_showed:
             if self.single:
+                if self.background is not None:
+                    screen.fill(self.background.get_rgba(), self.render.get_rect())
                 screen.blit(self.render, (self.x, self.y))
             else:
-                for i in range(len(self.renders)):
-                    screen.blit(self.renders[i], (self.x, self.y + 20 + (self.font.rendered_size(self.text.split("\n")[i])[1] * i + 2)))
+                for i, render in enumerate(self.renders):
+                    if self.background is not None:
+                        screen.fill(self.background.get_rgba(), render.get_rect())
+                    screen.blit(render, (self.x, self.y + 20 + render.get_rect().width * i + 2))
