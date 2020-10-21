355d32a8a6df2ce93d9586ae3d3f5f23a00279a2
LavaPower <lavapower84@gmail.com>
2020-10-21 14:36:15 +0200

    Fix Label

--- naos/graphics/widgets/label.py
+++ naos/graphics/widgets/label.py
@@ -23,10 +23,10 @@ class Label(Widget):
         if self.is_showed:
             if self.single:
                 if self.background is not None:
-                    screen.fill(self.background.get_rgba(), self.render.get_rect())
+                    screen.fill(self.background.get_rgba(), self.render.get_rect(x=self.x, y=self.y))
                 screen.blit(self.render, (self.x, self.y))
             else:
                 for i, render in enumerate(self.renders):
                     if self.background is not None:
-                        screen.fill(self.background.get_rgba(), render.get_rect())
-                    screen.blit(render, (self.x, self.y + 20 + render.get_rect().width * i + 2))
+                        screen.fill(self.background.get_rgba(), render.get_rect(x=self.x, y=self.y + (22 + render.get_rect().height) * i))
+                    screen.blit(render, (self.x, self.y + (22 + render.get_rect().height) * i))
