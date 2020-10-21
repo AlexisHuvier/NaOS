dc5fa5fcb577b6ace096a28f02c68481c2c6a3a1
LavaPower <lavapower84@gmail.com>
2020-10-22 01:32:03 +0200

    Create TextEditor

---
+++ naos/naos_core_programs/text_editor.py
@@ -0,0 +1,21 @@
+import os
+from math import *
+
+from naos.graphics.entities import Window
+from naos.graphics.widgets import *
+from naos.utils import Color
+
+class TextEditor(Window):
+    def __init__(self, naos):
+        super(TextEditor, self).__init__("Editeur de texte", 500, 300)
+        self.naos = naos
+        self.add_widget(TextEdit(10, 10, 480, 280))
+        
+
+program = {
+    "instance": TextEditor,
+    "infos": {
+        "name": "Editeur de texte",
+        "author": "LavaPower"
+    }
+}
